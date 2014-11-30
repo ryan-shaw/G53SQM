0ff065b4b3e0773f8a23773f7359e9ae0a5d0693
Ryan <ryan.shaw@min.vc>
2014-11-30 12:01:08 +0000

    Added unit test and updated build.xml

---
+++ src/vc/min/chat/Server/test/ServerTest.java
@@ -0,0 +1,14 @@
+package vc.min.chat.Server.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
