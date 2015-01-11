df9356553f6c56bc36a18b64eb8088fd39888d36
Ryan <ryan.shaw@min.vc>
2015-01-11 20:24:48 +0000

    Even better error messages

--- src/vc/min/chat/Shared/Packets/PacketHandler.java
+++ src/vc/min/chat/Shared/Packets/PacketHandler.java
@@ -63,6 +63,7 @@ public class PacketHandler implements IPacketHandler{
 			return packet;
 		} catch(InstantiationException | IllegalAccessException | NullPointerException e){
 			Logger.log(LogLevel.ERROR, "bad packet");
+			e.printStackTrace();
 		} catch(IOException e){
 			Logger.log(LogLevel.INFO, "client has disconnected");
 		}
